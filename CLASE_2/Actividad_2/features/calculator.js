dd50db16709427afb07e759b354ab23980c7b6ef
Camilo Andres Conrado Benavides <camiloconrado1@gmail.com>
2025-11-12 21:11:44 -0500

    feat(calc): add sum function

---
+++ CLASE_2/Actividad_2/features/calculator.js
@@ -0,0 +1,10 @@
+// Función para sumar dos números
+
+function sumar(a, b) {
+  return a + b;
+}
+
+// Prueba en consola
+console.log("Resultado de la suma:", sumar(5, 7));
+
+module.exports = { sumar };
